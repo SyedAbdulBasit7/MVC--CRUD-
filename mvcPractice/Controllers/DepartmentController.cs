93079368cbd139155b060d12df0f52e75077f5c4
Syed Abdul Basit <syedspy1444@gmail.com>
2020-12-24 18:21:14 +0500

    First Commit

---
+++ mvcPractice/Controllers/DepartmentController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvcPractice.Models;
+using System.Data.Entity;
+
+namespace mvcPractice.Controllers
+{
+   
+    public class DepartmentController : Controller
+    {
+        SignupLoginEntities db = new SignupLoginEntities();
+        // GET: Department
+        public ActionResult Index()
+        {
+            if (ModelState.IsValid == true)
+            {
+                var department= db.Departments.ToList();
+                return View(department);
+            }
+            
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Department dep)
+        {
+            if (ModelState.IsValid == true)
+            {
+                db.Departments.Add(dep);
+                var depData = db.SaveChanges();
+                if (depData > 0)
+                {
+                    TempData["message"] = "<script>alert('Department Add Successfully')</script>";
+                    return RedirectToAction("Index", "Department");
+                }
+                else
+                {
+                    TempData["message"] = "<script>alert('Department Add Failed!')</script>";
+                }
+
+            }
+            return View();
+        }
+        public ActionResult Edit(int id)
+        {
+                var selectDep=db.Departments.Where(model => model.depId == id).FirstOrDefault();
+            return View(selectDep);
+        }
+        [HttpPost]
+        public ActionResult Edit(Department dep)
+        {
+            if (ModelState.IsValid == true)
+            {
+                db.Entry(dep).State = EntityState.Modified;
+                var a=db.SaveChanges();
+                if (a > 0)
+                {
+                    TempData["editMessage"] = "<script>alert('Successfully Edit!')</script>";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["editMessage"] = "<script>alert('Failed Edit!')</script>";
+                }
+            }
+            return View();
+        }
+        public ActionResult Delete(int id)
+        {
+            var selectVar = db.Departments.Where(model => model.depId == id).FirstOrDefault();
+            db.Entry(selectVar).State = EntityState.Deleted;
+            int a = db.SaveChanges();
+            if (a > 0)
+            {
+                TempData["deleteMessage"] = "<script>alert('Successfully Edit!')</script>";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["deleteMessage"] = "<script>alert('Successfully Edit!')</script>";
+            }
+
+            return View();
+        }
+        //[HttpPost]
+        //public ActionResult Delete(Department dep)
+        //{
+           
+               
+            
+        //    return View();
+        //}
+
+    }
+}
---
+++ mvcPractice/Controllers/InforController.cs
@@ -0,0 +1,112 @@
+﻿using mvcPractice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace mvcPractice.Controllers
+{
+    public class InforController : Controller
+    {
+        SignupLoginEntities db = new SignupLoginEntities();
+        // GET: Infor
+        public ActionResult Index()
+        {
+            if (ModelState.IsValid == true) {
+                var employeeData=db.Employees.ToList();
+                ViewBag.myMessage = ("Data found");
+                return View(employeeData);
+            }
+            else
+            {
+                ViewBag.myMessage = ("Data Not found");
+                return View();
+            }
+
+           
+        }
+
+        public ActionResult Create()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Employee emp)
+        {
+            if (ModelState.IsValid == true)
+            {
+                db.Employees.Add(emp);
+                int a=db.SaveChanges();         
+                if (a > 0) {
+                    TempData["Insertmessage"] = "<script>alert('Added Successfully!')</script>";
+                    return RedirectToAction("Index", "Infor");
+                }
+                else
+                {
+                    TempData["Insertmessage"] = "<script>alert('Try Again Failed!')</script>";
+                }
+                
+            }
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var empSelect = db.Employees.Where(model => model.empId == id).FirstOrDefault();
+            return View(empSelect);
+        }
+        [HttpPost]
+        public ActionResult Edit(Employee emp)
+        {
+            if(ModelState.IsValid==true)
+            {
+                db.Entry(emp).State = EntityState.Modified;
+                var empEdit = db.SaveChanges();
+                if (empEdit > 0)
+                {
+                    TempData["editMessage"] = "<script>alert('Edit Successfully!')</script>";
+                    return RedirectToAction("Index", "Infor");
+                }
+                else
+                {
+                    TempData["editMessage"] = "<script>alert('Edit Failed!')</script>";
+                }
+            }
+            
+            return View();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var selectData = db.Employees.Where(model => model.empId == id).FirstOrDefault();
+            if (selectData != null)
+            {
+                db.Entry(selectData).State = EntityState.Deleted;
+                int a = db.SaveChanges();
+                if (a > 0)
+                {
+                    TempData["DeleteMessage"] = "<script>alert('Deleted Successfully!')</script>";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["DeleteMessage"] = "<script>alert('Deleted Failed!')</script>";
+                }
+            }
+         
+            return View();
+        }
+        //[HttpPost]
+        //public ActionResult Delete(Employee emp)
+        //{
+               
+        //        return RedirectToAction("Index");
+            
+        //}
+
+
+    }
+}
